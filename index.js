34ac02c6d0f2a09d48398c457977db740d60374d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-20 19:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener("keydown", function(e) {
 
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
-  let left parseInt(leftNumbers, 10);
+  let left = parseInt(leftNumbers, 10);
 
   if (left < 360) {
     dodger.style.left = `${left + 1}px`;
