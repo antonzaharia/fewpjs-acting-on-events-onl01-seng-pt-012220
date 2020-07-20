1e14e1e3882a715f0dc4ac5144e5761f195d0614
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-20 19:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keyright"), function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}
